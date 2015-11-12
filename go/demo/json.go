776a7180c4f02a9209d00a2b4e5d82c934d4d052
burakaydn <ydnburak@gmail.com>
2015-11-12 23:29:57 +0200

    init

---
+++ go/demo/json.go
@@ -0,0 +1,23 @@
+package demo
+
+import (
+	"fmt"
+	"encoding/json"
+)
+
+// START OMIT
+type User struct{
+	id int
+	name string
+}
+
+func main() {
+	data := []byte(`{"id": 123, "name": "jill"}`)
+	user := User{}
+	err := json.Unmarshal(data, &user)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s", user.name)
+}
+// END OMIT
