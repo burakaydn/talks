ef7c6f7a9a64574f89a8bacb81adb1fc8aee074c
burakaydn <ydnburak@gmail.com>
2015-11-13 23:40:10 +0200

    v1

--- go/demo/mux.go
+++ go/demo/mux.go
@@ -10,4 +10,10 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", handler)
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello Mux!")
 }
